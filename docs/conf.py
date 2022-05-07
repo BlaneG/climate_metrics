076323d3b86d6cb20205acde35f819fc166be226
Blane Grann <blanegrann@gmail.com>
2022-05-07 10:35:57 -0700

    Add myst_nb to extensions in config.py and docs/requirements.txt

--- docs/conf.py
+++ docs/conf.py
@@ -31,9 +31,10 @@ release = '2022.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'numpydoc',
+    'numpydoc',  # numpy formatted docstrings
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary'
+    'sphinx.ext.autosummary',
+    'myst_nb'  # for rendering .ipynb files
 ]
 
 # Add any paths that contain templates here, relative to this directory.
